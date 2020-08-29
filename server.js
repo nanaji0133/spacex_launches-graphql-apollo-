c3c3f6ecca44266d9a1a44dfd69eaf055d8b2cbc
nanaji0133 <nanajisanka5357@gmail.com>
2020-08-29 00:27:44 -0400

    launches with apollo

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const {graphqlHTTP} = require("express-graphql");
+const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema");
+const cors = require("cors");
 
 
 const app = express();
+
+app.use(cors());
 
 app.use(
     "/graphql",
