7ba8fa9de033c5b1e9a80e903219003ab9e5496a
nanaji0133 <nanajisanka5357@gmail.com>
2020-09-01 12:44:14 -0400

    react & apollo setup

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,17 +1,34 @@
 import gql from "graphql-tag";
 import React from 'react';
 import { Query } from "react-apollo";
+import LaunchItem from "./LaunchItem";
 
 
 const Launches = () =>
 {
     return (
         <div>
-            <h1>Launches</h1>
+            <h1 className="display-4 my-3">Launches</h1>
             <Query query={ LAUNCHES_QUERY }>
                 {
-                    ({ loading, error, data }) => 
-                        loading ? <h4>loading...</h4> : error ? console.log(error) : <p>testing</p> 
+                    ({ loading, error, data }) =>
+                        
+                        {
+                            if (loading) { return <h4>Loading...</h4>; }
+                            if (error) {return console.log(error)}
+                            console.log(data)
+
+                            return (
+                                <div>{
+                                data.launches.map(launch => 
+                                    <div key={launch.flight_number}> 
+                                        <LaunchItem launch={launch} /> 
+                                    </div>
+                                )
+                            }
+                            </div>
+                            )
+                        }
                 }
             </Query>
         </div>
